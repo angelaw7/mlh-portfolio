a69fb4dd9dc237a69f60e6fe89c2c86fe523a3e8
angela <wangela472@gmail.com>
2021-06-21 12:31:54 -0400

    moved website data to new file

---
+++ app/websiteData.py
@@ -0,0 +1,51 @@
+headerInfo = {
+    'img':'./static/img/fbcoverimg.jpg',
+    'name': 'Angela Wang',
+    'intro': 'Software and Biomedical Engineering Student, MLH Fellow'
+}
+
+aboutInfo = {
+    'shortParagraph': "Hey! My name is Angela and I'm a Software and Biomedical Engineering student at McMaster University. I'm passionate about medicine and technology and I hope to integrate both fields to one day work on developing innovative medical devices. I love to learn new things and I will never turn down an opportunity to continue developing my skills and exploring my interests. I'm currently participating in the Production Engineering Track of the MLH Fellowship this summer, where I'm learning  about Production/Site Reliability Engineering and DevOps. I'm also exploring Full-stack Development and I've recently taken an interest in Computer Vision and Machine Learning. \nIf anything here piques your interests, feel free to reach out! I'd love to connect and have a chat.",
+    'education': [
+        {
+            'schoolName': 'McMaster University',
+            'year': '2020 - 2025',
+            'description':'McMaster Developers Students Club, McMaster Start Coding, Medical Makers'
+        }
+    ],
+    'interest': ['Interest 1', 'Interest 2', 'Interest 3'],
+    'experience': [
+        {
+            'jobTitle': 'MLH Production Engineering Fellow',
+            'year': 'June 2021 - August 2021',
+            'jobDesc': 'Production Engineering, also known as Site Reliability Engineering & DevOps, is a hybrid between software & systems engineering that works across product & infrastructure to make sure services are reliable & scalable.'
+        },
+        {
+            'jobTitle': 'Makria Web Developer',
+            'year': 'June 2021 - August 2021',
+            'jobDesc': 'Updating and revising the Makria website using Wordpress. Managing plugings, Javascript interactions, eCommerce integrations.'
+        }
+    ],
+    'skill': [
+        {'name':'Python', 'icon':'fab fa-python'},
+        {'name':'Javascript','icon':'fab fa-js-square'},
+        {'name':'HTML/CSS','icon':'fab fa-html5'}
+        ]
+}
+
+projects = [
+    {
+        'title': 'Flask Web App',
+        'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eu sapien and lorem fermentum hendrerit quis mattis arcu. Nulla eget efficitur ex. Proin hendrerit ligula quis vehicula interdum.',
+        'date': '06/09/2021',
+        'img': './static/img/projects/web-dev.jpg',
+        'url': 'www.github.com',
+    },
+    {
+        'title': 'Machine Learning Project For Data Prediction',
+        'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eu sapien and lorem fermentum hendrerit quis mattis arcu. Nulla eget efficitur ex. Proin hendrerit ligula quis vehicula interdum.',
+        'date': '06/09/2021',
+        'img': './static/img/projects/machine-learning.jpg',
+        'url': 'www.github.com',
+    },
+]
