8cda406b58e166c7e54e12f2614ccff58e9eadaf
angela <wangela472@gmail.com>
2021-06-23 19:32:34 -0400

    updated /register endpoint

--- app/routes.py
+++ app/routes.py
@@ -4,7 +4,8 @@ from app.models import Blog
 import os
 import smtplib
 from app.websiteData import *
-
+from werkzeug.security import generate_password_hash
+from app.db import get_db
 
 # Routes
 @app.route('/')
@@ -93,3 +94,32 @@ def upload():
 def get_posts():
     posts = Blog.query.order_by(Blog.date_created).all()
     return posts
+
+@app.route('/register', methods=('GET', 'POST'))
+def register():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        db = get_db()
+        error = None
+
+        if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required.'
+        elif db.execute(
+            'SELECT id FROM user WHERE username = ?', (username,)
+        ).fetchone() is not None:
+            error = f"User {username} is already registered."
+
+        if error is None:
+            db.execute(
+                'INSERT INTO user (username, password) VALUES (?, ?)',
+                (username, generate_password_hash(password))
+            )
+            db.commit()
+            return f"User {username} created successfully"
+        else:
+            return error, 418
+
+    return "Register Page not yet implemented", 501
