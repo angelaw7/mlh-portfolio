ccce6b00385fd67c25e76f6a23ff7cb2463a9b17
angela <wangela472@gmail.com>
2021-07-02 02:02:27 -0400

    updated login and register endpoints

--- app/routes.py
+++ app/routes.py
@@ -107,17 +107,13 @@ def register():
             error = 'Username is required.'
         elif not password:
             error = 'Password is required.'
-        elif db.execute(
-            'SELECT id FROM user WHERE username = ?', (username,)
-        ).fetchone() is not None:
+        elif UserModel.query.filter_by(username=username).first() is not None:
             error = "User {username} is already registered."
-
+        
         if error is None:
-            db.execute(
-                'INSERT INTO user (username, password) VALUES (?, ?)',
-                (username, generate_password_hash(password))
-            )
-            db.commit()
+            new_user = UserModel(username, generate_password_hash(password))
+            db.session.add(new_user)
+            db.session.commit()
             return "User {username} created successfully"
         else:
             return error, 418
@@ -132,9 +128,7 @@ def login():
         password = request.form.get('password')
         db = get_db()
         error = None
-        user = db.execute(
-            'SELECT * FROM user WHERE username = ?', (username,)
-        ).fetchone()
+        user = UserModel.query.filter_by(username=username).first()
 
         if user is None:
             error = 'Incorrect username.'
