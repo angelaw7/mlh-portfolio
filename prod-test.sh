918b0e6330b1f3c296fda8b1833df3726fb6f7d1
Angela <wangela472@gmail.com>
2021-07-14 18:08:17 -0400

    Update endpoints

--- prod-test.sh
+++ prod-test.sh
@@ -6,8 +6,6 @@ while read endpoint; do
   response=$(curl -s -o /dev/null -w "%{http_code}" $domain$endpoint)
   if [[ $response == "200" ]]; then
     echo "$domain$endpoint | Success"
-  elif [[ $endpoint == "login" || "register" ]] && [[ $response == "501" ]]; then
-    echo "$domain$endpoint | Not yet implemented"
   else
     echo "$domain$endpoint | Error"
     exit 1
