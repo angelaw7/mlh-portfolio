485951f7f98e8c23f37413f96b956807738d5028
Angela <wangela472@gmail.com>
2021-08-04 12:07:55 -0400

    Fix Flake8 linting issues

--- app/website_data.py
+++ app/website_data.py
@@ -5,12 +5,26 @@ header_info = {
 }
 
 about_info = {
-    "shortParagraph": "Hey! My name is Angela and I'm a Software and Biomedical Engineering student at McMaster University. I'm passionate about medicine and technology and I hope to integrate both fields to one day work on developing innovative medical devices. I love to learn new things and I will never turn down an opportunity to continue developing my skills and exploring my interests. I'm currently participating in the Production Engineering Track of the MLH Fellowship this summer, where I'm learning  about Production/Site Reliability Engineering and DevOps. I'm also exploring Full-stack Development and I've recently taken an interest in Computer Vision and Machine Learning. \nIf anything here piques your interests, feel free to reach out! I'd love to connect and have a chat.",
+    "shortParagraph": "Hey! My name is Angela and I'm a Software and \
+        Biomedical Engineering student at McMaster University. I'm passionate \
+        about medicine and technology and I hope to integrate both fields to \
+        one day work on developing innovative medical devices. I love to \
+        learn new things and I will never turn down an opportunity to \
+        continue developing my skills and exploring my interests. \
+        I'm currently participating in the Production Engineering \
+        Track of the MLH Fellowship this summer, where I'm learning  \
+        about Production/Site Reliability Engineering and DevOps. I'm \
+        also exploring Full-stack Development and I've recently taken an \
+        interest in Computer Vision and Machine Learning. \nIf anything \
+        here piques your interests, feel free to reach out! I'd love to \
+        connect and have a chat.",
     "education": [
         {
             "schoolName": "McMaster University",
             "year": "2020 - 2025",
-            "description": "Medical Makers, McMaster Developers Students Club, McMaster Start Coding",
+            "description":
+                "Medical Makers, \
+                McMaster Developers Students Club, McMaster Start Coding",
         }
     ],
     "interest": [
@@ -22,12 +36,17 @@ about_info = {
         {
             "jobTitle": "MLH Production Engineering Fellow",
             "year": "June 2021 - August 2021",
-            "jobDesc": "Production Engineering, also known as Site Reliability Engineering & DevOps, is a hybrid between software & systems engineering that works across product & infrastructure to make sure services are reliable & scalable.",
+            "jobDesc": "Production Engineering, also known as Site Reliability \
+                Engineering & DevOps, is a hybrid between software & systems \
+                engineering that works across product & infrastructure to \
+                make sure services are reliable & scalable.",
         },
         {
             "jobTitle": "Makria Web Developer",
             "year": "June 2021 - August 2021",
-            "jobDesc": "Updating and revising the Makria website using Wordpress. Managing plugings, Javascript interactions, eCommerce integrations.",
+            "jobDesc": "Updating and revising the Makria website using \
+                Wordpress. Managing plugings, Javascript interactions, \
+                eCommerce integrations.",
         },
     ],
     "skill": [
@@ -39,11 +58,12 @@ about_info = {
 
 projects = [
     {
-        "title': 'MLH Fellowship - Personal Website",
-        "description': 'A personal website built using the Flask framework for the Production Engineering MLH Fellowship",
-        "date': 'June 2021 - present",
-        "img': './static/img/projects/web_cover.png",
-        "url': 'https://github.com/angelaw7/mlh-portfolio",
+        "title": "MLH Fellowship - Personal Website",
+        "description": "A personal website built using the Flask framework for \
+            the Production Engineering MLH Fellowship",
+        "date": "June 2021 - present",
+        "img": "./static/img/projects/web_cover.png",
+        "url": "https://github.com/angelaw7/mlh-portfolio",
     },
     {
         "title": "Discord Bot",
